b1ac8d54578a0eed601c03ddb02fdc3091c098cc
Jobel Racines <43741652+jobrac@users.noreply.github.com>
2023-03-06 13:43:02 +0000

    update

--- api/index.js
+++ api/index.js
@@ -1,12 +1,21 @@
-// export const config = {
-//   runtime: 'edge',
-// }
+export const config = {
+  runtime: 'edge',
+}
 
 export default async function handler(req) {
  
   
   console.log(req);
 
-  return new Response(req
+  return new Response(
+    JSON.stringify({
+      message: req.headers['x-vercel-forwarded-for'],
+    }),
+    {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+    }
   )
 }
