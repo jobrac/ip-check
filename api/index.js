6a838de29143533d7f9ab4074392548a9435a03e
Jobel Racines <43741652+jobrac@users.noreply.github.com>
2023-03-06 13:40:41 +0000

    update

--- api/index.js
+++ api/index.js
@@ -1,21 +1,12 @@
-export const config = {
-  runtime: 'edge',
-}
+// export const config = {
+//   runtime: 'edge',
+// }
 
 export default async function handler(req) {
  
   
   console.log(req);
 
-  return new Response(
-    JSON.stringify({
-      message: 'Hello, world!',
-    }),
-    {
-      status: 200,
-      headers: {
-        'content-type': 'application/json',
-      },
-    }
+  return new Response(req
   )
 }
